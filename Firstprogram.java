277913b25558552d3a4505ae96f58b4e9223c869
akc007 <aniketchoubey91@gmail.com>
2024-06-06 20:23:24 +0000

    Entered the new files in the repository. This is for learning purpose.

---
+++ Firstprogram.java
@@ -0,0 +1,21 @@
+import java.lang.*;
+import java.util.*;
+
+public class OperateNumbers(a,b)
+{
+    int a= new int();
+    int b= new int();
+}
+
+public static void main(String [] args)
+{
+OperateNumbers op=new OperateNumbers(4,5);
+int res=arr[];
+int op=[+,-,*,/,%]
+int n=5;
+for(int i=0;i<n;i++)
+{
+    res[i]=a+op[i]+b;
+}
+System.out.println(res);
+}
